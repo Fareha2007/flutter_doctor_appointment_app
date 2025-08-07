20068b54acd60ed03a58ac9d76624822bb984d0a
Fareha <tfareha.123@gmail.com>
2025-08-07 22:59:20 +0530

    Initial commit for doctor app

---
+++ android/app/src/main/kotlin/com/example/flutter_doctor_app_fixed/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_doctor_app_fixed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_doctor_app_fixed/screens/appointment_confirmation_screen.dart';
+import 'package:flutter_doctor_app_fixed/screens/booking_screen.dart';
+import 'package:flutter_doctor_app_fixed/screens/my_appointments_screen.dart';
+
+void main() {
+  print("🔥 Launching Doctor Appointment App");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Doctor Appointment App',
+
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const AppointmentConfirmationScreen(),
+      routes: {
+        '/book': (context) => BookingScreen(),
+        '/schedule': (context) => MyAppointmentsScreen(),
+        '/confirmation': (context) => AppointmentConfirmationScreen(),
+      },
+    );
+  }
+}
